be90d675f52f990b5acba3fb4bc7333eb45af871
Leandro Matos <leandro.matos@live.com>
2023-08-31 14:20:38 -0300

    Aula 3 - Operadores Unários

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,51 +1,22 @@
+//Operadores Unários
+        //Eles realizam alguns trabalhos básicos como incrementar, decrementar, inverter valores numéricos e booleanos.
+        //(+) Operador unário de valor positivo
+        //(-) Operador unário de valor negativo
+        //(++) Operador unário de incremento de valor
+        //(--) Operador unário de valor negativo
+        //(!) Operador unário lógico de negação
+
 public class Operadores {
     public static void main(String[] args) {
-    //classe Operadores.java
+        int numero = 5;
 
-        //String nome = "Leandro";
-        //int idade = 22;
-        //double peso = 68.5;
-        //char sexo = 'M';
-        //boolean doadorOrgao = true;
-        //Date dataNascimento = new Date();
+        numero = - numero;
 
-    //Operadores Aritméticos
-    // + (adição), - (subtração), * (multiplicação) e / (divisão)
+        System.out.println(numero);
 
-        //double soma = 10.5 + 15.7;
-        //int subtracao = 113 - 25;
-        //int multiplicacao = 20 * 7;
-        //int divisao = 15 / 3;
-        //int modulo = 18 % 3;
-        //double resultado = (10 * 7) + (20 / 4);
+        numero = numero * -1;
 
-    //ATENÇÃO! O Operador de adiçã(+), quando utilizado em variáveis do tipo texto, realizará a "concatenação de textos".
-        //String nomeCompleto = "LINGUAGEM" + "JAVA";
-        //System.out.println(nomeCompleto);
+        System.out.println(numero);
 
-    //qual o resutado das impressoes abaixo
-
-        String concatenacao ="?";
-
-        concatenacao = 1+1+1+"1";
-
-        System.out.println(concatenacao);
-
-        concatenacao = 1+"1"+1+1;
-
-        System.out.println(concatenacao);
-
-        concatenacao = 1+"1"+1+"1";
-
-        System.out.println(concatenacao);
-
-        concatenacao = "1"+1+1+1;
-
-        System.out.println(concatenacao);
-
-        concatenacao = "1"+(1+1+1);
-        
-        System.out.println(concatenacao);
-        
     }
 }
