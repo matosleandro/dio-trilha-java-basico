645cdd332ddac69eeb1eb3aff2388e58eb1a15a3
Leandro Matos <leandro.matos@live.com>
2023-09-05 10:50:21 -0300

    Aula 6 - Operadores Relacionais

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,21 +1,32 @@
 public class Operadores {
     public static void main(String[] args) {
-        int a, b;
+/* OPERADORES RELACIONAIS */
+        String nomeUm = "Leandro";
+        String nomeDois = new String("Leandro");
 
-        a = 5;
-        b = 6;
-        /*String resultado ="";*/
-        /* EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE */
-        /* OPERADOR TERNARIO */
-        /*if(a==b)
-            resultado = "verdadeiro";
-        else
-            resultado = "falso";*/
-
-            /*String resultado = a == b ? "verdadeiro" : "falso";*/
-            int resultado = a == b ? 1 : 0;
+        System.out.println(nomeUm.equals(nomeDois));
 
 
-            System.out.println(resultado);
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        if(numero1 < numero2){ //true
+            System.out.println("a nossa condição é verdadeira");
+        }
+
+
+        System.out.println("numeroUm é igual a numeroDois?" + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("numeroUm é diferente a numeroDois?" + simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println("numeroUm é maior que numeroDois?" + simNao);
+
     }
 }
