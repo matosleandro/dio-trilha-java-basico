2c76eff93d41901e2c8f9423a69937400008054c
Leandro Matos <leandro.matos@live.com>
2023-09-05 11:03:27 -0300

    Aula 7 - Operadores Logicos

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,32 +1,20 @@
+/* OPERADORES LOGICOS */
+//Os operadores lógicos representam o recurso que nos permite criar expressões lógicas maiores a partir da junção de duas ou mais expressões.
+
 public class Operadores {
     public static void main(String[] args) {
-/* OPERADORES RELACIONAIS */
-        String nomeUm = "Leandro";
-        String nomeDois = new String("Leandro");
+        boolean condicao1=true;
 
-        System.out.println(nomeUm.equals(nomeDois));
+        boolean condicao2=false;
 
-
-
-        int numero1 = 1;
-        int numero2 = 2;
-
-        boolean simNao = numero1 == numero2;
-
-        if(numero1 < numero2){ //true
-            System.out.println("a nossa condição é verdadeira");
+        if(condicao1 && (7 > 4)){
+            System.out.println("as duas condições são verdadeiras");
         }
 
+        if(condicao1 || condicao2){
+            System.out.println("Uma das condições é verdadeira");
+        }
 
-        System.out.println("numeroUm é igual a numeroDois?" + simNao);
-
-        simNao = numero1 != numero2;
-
-        System.out.println("numeroUm é diferente a numeroDois?" + simNao);
-
-        simNao = numero1 > numero2;
-
-        System.out.println("numeroUm é maior que numeroDois?" + simNao);
-
+        System.out.println("fim");
     }
 }
